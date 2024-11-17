57b2458cec04d4c395a009f1fecf8a2f9ebf90a5
Casey <soulcasey@gmail.com>
2024-11-17 18:30:03 -0500

    Set motor platform

--- Motor.py
+++ Motor.py
@@ -1,68 +1,74 @@
-import RPi.GPIO as GPIO
+import platform
 
-if GPIO.getmode() != GPIO.BOARD:
-    GPIO.setmode(GPIO.BOARD)
+# Check if the code is running on a Raspberry Pi or Windows
+is_raspberry_pi = platform.system() == "Linux" and "raspberrypi" in platform.node().lower()
 
-speed = 40
+if is_raspberry_pi:
+    import RPi.GPIO as GPIO
 
-class Motor:
-    def __init__(self, pwm_pin, dir_pin1, dir_pin2):
-        self.pwm_pin = pwm_pin
-        self.dir_pin1 = dir_pin1
-        self.dir_pin2 = dir_pin2
-        GPIO.setup(self.pwm_pin, GPIO.OUT)
-        GPIO.setup(self.dir_pin1, GPIO.OUT)
-        GPIO.setup(self.dir_pin2, GPIO.OUT)
-        self.motor = GPIO.PWM(self.pwm_pin, 1000)
+    if GPIO.getmode() != GPIO.BOARD:
+        GPIO.setmode(GPIO.BOARD)
 
-    def move(self, forward: bool):
-        GPIO.output(self.dir_pin1, forward)
-        GPIO.output(self.dir_pin2, not forward)
-        self.motor.ChangeDutyCycle(speed)
+    speed = 40
 
-    def stop(self):
-        GPIO.output(self.dir_pin1, False)
-        GPIO.output(self.dir_pin2, False)
-        self.motor.ChangeDutyCycle(0)
-        
-motor_1 = Motor(pwm_pin=11, dir_pin1=13, dir_pin2=15)
-motor_2 = Motor(pwm_pin=22, dir_pin1=16, dir_pin2=18)
-motor_3 = Motor(pwm_pin=36, dir_pin1=31, dir_pin2=29)
+    class Motor:
+        def __init__(self, pwm_pin, dir_pin1, dir_pin2):
+            self.pwm_pin = pwm_pin
+            self.dir_pin1 = dir_pin1
+            self.dir_pin2 = dir_pin2
+            GPIO.setup(self.pwm_pin, GPIO.OUT)
+            GPIO.setup(self.dir_pin1, GPIO.OUT)
+            GPIO.setup(self.dir_pin2, GPIO.OUT)
+            self.motor = GPIO.PWM(self.pwm_pin, 1000)
 
-def moveMotor1(forward: bool):
-    motor_1.move(forward)
+        def move(self, forward: bool):
+            GPIO.output(self.dir_pin1, forward)
+            GPIO.output(self.dir_pin2, not forward)
+            self.motor.ChangeDutyCycle(speed)
 
-def stopMotor1():
-    motor_1.stop()
+        def stop(self):
+            GPIO.output(self.dir_pin1, False)
+            GPIO.output(self.dir_pin2, False)
+            self.motor.ChangeDutyCycle(0)
 
-def moveMotor2(forward: bool):
-    motor_2.move(forward)
+    motor_1 = Motor(pwm_pin=11, dir_pin1=13, dir_pin2=15)
+    motor_2 = Motor(pwm_pin=22, dir_pin1=16, dir_pin2=18)
+    motor_3 = Motor(pwm_pin=36, dir_pin1=31, dir_pin2=29)
 
-def stopMotor2():
-    motor_2.stop()
+    def moveMotor1(forward: bool):
+        motor_1.move(forward)
 
-def moveMotor3(forward: bool):
-    motor_3.move(forward)
+    def stopMotor1():
+        motor_1.stop()
 
-def stopMotor3():
-    motor_3.stop()
+    def moveMotor2(forward: bool):
+        motor_2.move(forward)
 
+    def stopMotor2():
+        motor_2.stop()
 
-# def moveMotor1(forward: bool):
-#     print("Motor 1 Forward") if forward else print("Motor 1 Backward")
+    def moveMotor3(forward: bool):
+        motor_3.move(forward)
 
-# def stopMotor1():
-#     print("Motor 1 Stop")
+    def stopMotor3():
+        motor_3.stop()
 
-# def moveMotor2(forward: bool):
-#     print("Motor 2 Forward") if forward else print("Motor 2 Backward")
+else:
+    # For Windows, simulate the motor behavior (no GPIO interaction)
+    def moveMotor1(forward: bool):
+        print("Motor 1 Forward") if forward else print("Motor 1 Backward")
 
-# def stopMotor2():
-#     print("Motor 2 Stop")
+    def stopMotor1():
+        print("Motor 1 Stop")
 
-# def moveMotor3(forward: bool):
-#     print("Motor 3 Forward") if forward else print("Motor 3 Backward")
+    def moveMotor2(forward: bool):
+        print("Motor 2 Forward") if forward else print("Motor 2 Backward")
 
-# def stopMotor3():
-#     print("Motor 3 Stop")
+    def stopMotor2():
+        print("Motor 2 Stop")
 
+    def moveMotor3(forward: bool):
+        print("Motor 3 Forward") if forward else print("Motor 3 Backward")
+
+    def stopMotor3():
+        print("Motor 3 Stop")
