4924e3d4be7d641862aea17d6496884de091a2a3
Casey <82072694+soulcasey@users.noreply.github.com>
2024-11-21 18:37:29 -0500

    Adjust test time

--- TestMotor.py
+++ TestMotor.py
@@ -3,11 +3,11 @@ import time
 
 try:
     motor.moveMotor2(True)
-    time.sleep(6.65)
+    time.sleep(6.6)
     motor.stopMotor2()
     time.sleep(2)
     motor.moveMotor2(False)
-    time.sleep(6.65)
+    time.sleep(6.6)
     motor.stopMotor2()
     time.sleep(2)
 
