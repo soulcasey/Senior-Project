618d8023d43441a55cb89eebc57fe2dbfbd17ac3
Casey <82072694+soulcasey@users.noreply.github.com>
2024-11-21 18:43:21 -0500

    Adjust motor test time

--- TestMotor.py
+++ TestMotor.py
@@ -2,18 +2,20 @@ import Motor as motor
 import time
 
 try:
-    motor.moveMotor2(True)
-    time.sleep(6.4)
-    motor.stopMotor2()
-    time.sleep(2)
-    motor.moveMotor2(True)
-    time.sleep(3.2)
-    motor.stopMotor2()
-    time.sleep(2)
-    motor.moveMotor2(True)
-    time.sleep(3.2)
-    motor.stopMotor2()
-    time.sleep(2)
+    for i in range(10):
+        motor.moveMotor2(True)
+        time.sleep(6.4)
+        motor.stopMotor2()
+        time.sleep(2)
+
+        motor.moveMotor2(True)
+        time.sleep(3.2)
+        motor.stopMotor2()
+        time.sleep(2)
+        motor.moveMotor2(True)
+        time.sleep(3.2)
+        motor.stopMotor2()
+        time.sleep(2)
 
 except Exception as e:
     motor.stopAllMotors()
