5d4482d2f0daaacabdcde2d12a519a5463b017b9
Casey <soulcasey@gmail.com>
2024-11-24 23:40:11 -0500

    Adjust sequence

--- TestSequence.py
+++ TestSequence.py
@@ -3,3 +3,11 @@ import time
 
 motor.moveMotor1(True)
 time.sleep(1.5)
+motor.moveMotor1(False)
+time.sleep(1.5)
+motor.moveMotor1(False)
+time.sleep(1.5)
+motor.moveMotor1(True)
+time.sleep(3)
+motor.moveMotor1(False)
+time.sleep(1.5)
