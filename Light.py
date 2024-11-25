932da4aee482c1248684ffad6fd31a3d42f57aa7
Casey <soulcasey@gmail.com>
2024-11-24 23:10:13 -0500

    Add light

--- Light.py
+++ Light.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
-camera_light = 24
-warning_light = 26
+camera_light = 29
+warning_light = 37
 
 if (GPIO.getmode() != GPIO.BOARD):
     GPIO.setmode(GPIO.BOARD)
---
+++ TestLight.py
@@ -0,0 +1,20 @@
+from Controller import ButtonType, Controller
+import Light as light
+
+controller = Controller()
+
+controller.set_button_press_action(ButtonType.A, lambda: light.cameraLight(True))
+controller.set_button_press_action(ButtonType.B, lambda: light.warningLight(True))
+
+# Set button release actions to stop motors
+controller.set_button_release_action(ButtonType.A, lambda: light.cameraLight(False))
+controller.set_button_release_action(ButtonType.B, lambda: light.warningLight(False))
+
+
+# Main loop
+try:
+    while True:
+        controller.loop()
+
+finally:
+    controller.exit()
